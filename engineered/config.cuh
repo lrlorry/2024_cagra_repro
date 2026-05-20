581760182ab8440b410a02f4cfeec2db07cceb45
Rui Luo <u8076655@anu.edu.au>
2026-05-20 13:53:53 +1000

    aaa

--- engineered/config.cuh
+++ engineered/config.cuh
@@ -6,11 +6,11 @@ constexpr int kInitialDegree = 128;  // cuVS: intermediate_graph_degree=128
 constexpr int kGraphDegree = 64;     // cuVS: graph_degree=64
 constexpr int kNnDescentIters = 8;
 constexpr int kTopK = 10;
-constexpr int kInternalTopM = 64;
+constexpr int kInternalTopM = 256;
 constexpr int kSingleCtaSearchWidth = 2;
 constexpr int kMaxSearchIters = 32;
 constexpr int kBlockThreads = 128;
-constexpr int kHashSize = 256;
+constexpr int kHashSize = 1024;
 constexpr int kMultiCtaPerQuery = 4;
 
 constexpr unsigned kParentMask = 0x80000000U;
